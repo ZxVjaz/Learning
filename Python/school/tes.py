75c934f37b96d015e48fa362d0209679d711e4bf
Zenith <lokilaka774@gmail.com>
2025-10-09 11:26:30 +0700

    update

--- Python/school/tes.py
+++ Python/school/tes.py
@@ -2,18 +2,24 @@
 L = 0
 P = 0
 Jumlah_Semua_Siswa = 0
-while True:
-    Nama = input("Masukkan Nama Anda : ").lower()
-    Jenis_Kelamin = input("Masukkan Jenis Kelamin Anda (L/P) : ").lower()
-    if Jenis_Kelamin == "l":
-        L += 1
-        print("Nama Anda Adalah : ", Nama)
-        print("Jumlah Siswa Laki-Laki : ", L, "\nJumlah Siswa Perempuan : ", P)
-    else:
-        P += 1
-        print("Nama Anda Adalah : ", Nama)
-        print("Jumlah Siswa Laki-Laki : ", L, "\nJumlah Siswa Perempuan : ", P)
-    if Nama == "reno":
-        break
-#selesai
+Nama = ""
 
+while Nama != "reno":
+    Nama = input("Masukkan Nama Anda : ").lower()
+    if Nama == "reno":
+        te = " "
+    else:
+        Jenis_Kelamin = input("Masukkan Jenis Kelamin Anda (L/P) : ").lower()
+        
+    if Jenis_Kelamin == "l":
+            L += 1
+    else:
+            P += 1
+            
+print("Nama Anda Adalah :", Nama)
+print("Jumlah Siswa Laki-Laki :", L)
+print("Jumlah Siswa Perempuan :", P)
+print("-" * 30)
+
+print("Jumlah Semua Siswa :", L + P)
+#selesai

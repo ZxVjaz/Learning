8f92420d4cbc4f0275656d2ffd66bf9a8c0bec00
Zenith <lokilaka774@gmail.com>
2025-10-16 04:09:24 +0700

    small update on Python/school/Program_Entry_Guru.py

--- Python/school/Program_Entry_Guru.py
+++ Python/school/Program_Entry_Guru.py
@@ -1,29 +1,38 @@
-#Pembukaan
-print('=' * 30)
-Nama_Guru = input('Nama Guru:').lower().replace(' ', '')
-Nama_Mapel = input('Nama Mapel:')
-Jurusan = input('Jurusan:')
-print('=' * 30)
+#perulangan
+while True:
+    #Pembukaan
+    print('=' * 30)
+    Nama_Guru = input('Nama Guru:').lower().replace(' ', '')
+    Nama_Mapel = input('Nama Mapel:')
+    Jurusan = input('Jurusan:')
+    print('=' * 30)
 
-#Proses
+    #Proses
 
-if Nama_Guru == 'ramdani':
-    Nama_Guru = 'Ramdani'
-    Hari = 'Senin'
-elif Nama_Guru == 'atiks':
-    Nama_Guru = 'Atik S'
-    Hari = 'Selasa'
-elif Nama_Guru == 'indri':
-    Nama_Guru = 'Indri'
-    Hari = 'Rabu'
-elif Nama_Guru == 'faizal':
-    Nama_Guru = 'Faizal'
-    Hari = 'Kamis'
-elif Nama_Guru == 'tholib':
-    Nama_Guru = 'Tholib'
-    Hari = 'Jum\'at'
-else:
-    Hari = 'Tidak diketahui'
+    if Nama_Guru == 'ramdani':
+        Nama_Guru = 'Ramdani'
+        Hari = 'Senin'
+    elif Nama_Guru == 'atiks':
+        Nama_Guru = 'Atik S'
+        Hari = 'Selasa'
+    elif Nama_Guru == 'indri':
+        Nama_Guru = 'Indri'
+        Hari = 'Rabu'
+    elif Nama_Guru == 'faizal':
+        Nama_Guru = 'Faizal'
+        Hari = 'Kamis'
+    elif Nama_Guru == 'tholib':
+        Nama_Guru = 'Tholib'
+        Hari = 'Jum\'at'
+    else:
+        Hari = 'Tidak diketahui'
 
-#Output
-print(Nama_Guru, Nama_Mapel, Jurusan, Hari)
+    #Output
+    print(Nama_Guru, Nama_Mapel, Jurusan, Hari)
+
+    print('=' * 30)
+    
+    ulang = input('Ulang (y/t)?').lower()
+    if ulang == 't':
+        break
+#selesai
